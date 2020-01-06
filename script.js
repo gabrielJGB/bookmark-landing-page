9577d6bb69af6bf030b8cbb215510fb78d6ab6d1
gabrielJGB <brunengo.gabriel@gmail.com>
2020-01-05 22:12:18 -0300

    agrega menu responsive

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const $button = document.querySelector('.icon')
+        const $header = document.querySelector('.header');
+        const $logo = document.querySelector('.logo');
 
+        $button.addEventListener('click', myFunction);
+
+        function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+                $header.style['background-color'] = "rgba(8, 5, 63, 0.74)";
+                $logo.style.filter = "invert(100%)";
+            } else {
+                x.className = "topnav";
+                $header.style['background-color'] = "unset";
+                $logo.style.filter = "invert(0%)";
+            }
+        }
