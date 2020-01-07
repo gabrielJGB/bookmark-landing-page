d67c100dba483a9ee98f40a2a399ca73c8a07405
gabrielJGB <brunengo.gabriel@gmail.com>
2020-01-07 19:32:36 -0300

    agrega seccion

--- script.js
+++ script.js
@@ -17,3 +17,27 @@ function cambiarAnchoMenu(){
 var x = window.matchMedia("(max-width: 800px)");
 x.addListener(cambiarAnchoMenu);
 
+document.querySelector('.tab-1').style['border-bottom'] = 'solid 5px rgb(218, 63, 63)';
+
+function tabSelector(numeroTab){
+    const $tabSelector = document.querySelectorAll('.tab-container a');
+    
+    $tabSelector.forEach(function(i){
+        i.style['border-bottom'] = "";
+    })
+
+    const $x = document.querySelector('.tab-'+numeroTab);
+   $x.style['border-bottom'] = 'solid 5px rgb(218, 63, 63)';
+
+   const $tabs = document.querySelectorAll('.tab');
+   
+   $tabs.forEach(function(i){
+        i.style.display='none';
+   })
+
+   const $tabMostrar = document.querySelector('.tab'+numeroTab);
+   $tabMostrar.style.display = 'flex';
+}
+
+
+
